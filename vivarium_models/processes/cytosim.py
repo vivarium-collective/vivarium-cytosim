120898a1fe2cc1de939a906492ab985ee6831ddc
Ryan Spangler <ryan.spangler@gmail.com>
2022-01-25 14:03:25 -0800

    cytosim generating output and reporting to process

--- vivarium_models/processes/cytosim.py
+++ vivarium_models/processes/cytosim.py
@@ -19,6 +19,7 @@ from pathlib import Path
 import subprocess
 from vivarium_models.library.schema import fibers_schema
 from vivarium_models.data.fibers import initial_fibers
+import vivarium_models.library.go_sim as go_sim
 
 NAME = "CYTOSIM"
 
@@ -42,15 +43,20 @@ def fiber_section(id, fiber):
 
 class CytosimProcess(Process):
     defaults = {
+        'model_name': 'cytosim_model',
         'internal_timestep': 0.001,
         'cytosim_template': "cytosim-buckling.cym",
         'cell_radius': 5,
         "input_directory": "in/",
         "output_directory": "out/",
-        'cytosim_executable': "../cytosim/bin/sim"}
+        'cytosim_sim': "../cytosim/bin/sim",
+        'cytosim_report': "../cytosim/bin/report"}
 
     def __init__(self, parameters=None):
         super().__init__(parameters)
+
+        self.input_path = Path(self.parameters['input_directory'])
+        self.output_path = Path(self.parameters['output_directory'])
 
     def ports_schema(self):
         return fibers_schema()
@@ -71,6 +77,29 @@ class CytosimProcess(Process):
             filament_section='\n\n\n'.join(fiber_sections),
             simulation_time=int(timestep/self.parameters['internal_timestep']),
         )
+
+        config_path = self.input_path / self.parameters['cytosim_template']
+        with open(config_path, "w") as cytosim_file:
+            cytosim_file.write(cytosim_config)
+
+        go_sim.local_run(
+            os.path.abspath(self.parameters['cytosim_sim']),
+            config_path,
+            self.parameters['model_name'],
+            self.output_path,
+        )
+
+        report_input = self.output_path / self.parameters['model_name'] / 'objects.cmo'
+        report_command = [
+            os.path.abspath(self.parameters['cytosim_report']),
+            "fiber:points",
+            f"input={report_input}"
+        ]
+
+        cytosim_process = subprocess.Popen(report_command, stdout=subprocess.PIPE)
+        output, error = cytosim_process.communicate()
+
+        print(output.decode("utf-8"))
 
         import ipdb; ipdb.set_trace()
 
