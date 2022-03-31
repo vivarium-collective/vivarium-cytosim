5f019a5d256fa3d9296578b79fe3249d40593e0a
Blair Lyons <blair208@gmail.com>
2022-03-30 17:13:35 -0700

    export CytosimProcess in __init__.py

--- vivarium_cytosim/__init__.py
+++ vivarium_cytosim/__init__.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for Vivarium Cytosim."""
+"""Top-level package for vivarium-cytosim."""
+
+__author__ = "Blair Lyons"
+__email__ = "blair208@gmail.com"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .processes.cytosim import CytosimProcess  # noqa: F401
